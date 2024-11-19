72471656d5307d3a4f3dcad41ff0fccdd79679d1
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-19 23:26:09 +0330

    add website hero slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { HeroComponent } from './features/home/hero/hero.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HeroComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
+    CarouselModule,
     NgIconsModule.withIcons({ featherPhoneCall, featherChevronDown }),
   ],
   providers: [provideClientHydration()],
--- src/app/features/home/hero/hero.component.ts
+++ src/app/features/home/hero/hero.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
 })
 export class HeroComponent {
-
+  heroSliderOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      740: {
+        items: 1,
+      },
+      940: {
+        items: 1,
+      },
+    },
+    nav: false,
+  };
 }
