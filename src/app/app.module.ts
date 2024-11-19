c7e144a751e5504dbce7182d2d07a8d4f5aec02a
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-19 20:48:47 +0330

    add navbar component to the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './features/home/home.component';
 import { HeroComponent } from './features/home/hero/hero.component';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeroComponent],
+  declarations: [AppComponent, HomeComponent, HeroComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
