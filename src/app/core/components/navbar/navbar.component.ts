843f87a07bfcfa79e9ca0edc9c638baa8bfe7c4e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-19 21:20:08 +0330

    style the navbar component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  navbarMenus = [
+    {
+      title: 'Home',
+    },
+    {
+      title: 'Services',
+      submenus: [
+        {
+          title: 'Service 01',
+        },
+        {
+          title: 'Service 02',
+        },
+        {
+          title: 'Service 03',
+        },
+      ],
+    },
+    {
+      title: 'Blogs',
+    },
+    {
+      title: 'About Us',
+    },
+    {
+      title: 'Contact Us',
+    },
+  ];
 }
