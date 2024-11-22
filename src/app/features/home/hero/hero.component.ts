f752cb5f1a65f0c6d772462267a088e54ca0ed12
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-22 16:53:53 +0330

    set options for swiper slider

--- src/app/features/home/hero/hero.component.ts
+++ src/app/features/home/hero/hero.component.ts
@@ -1,9 +1,65 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { SwiperContainer } from 'swiper/element';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
-  encapsulation: ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.Emulated,
 })
-export class HeroComponent {}
+export class HeroComponent implements AfterViewInit {
+  @ViewChild('swiperEl', { static: false }) swiperEl?: ElementRef;
+  config: SwiperOptions = {
+    navigation: false,
+    slidesPerView: 1,
+    pagination: {
+      clickable: true,
+    },
+    injectStyles: [
+      `
+      .swiper-pagination{
+      position: absolute;
+      bottom: 4vh !important;
+      }
+
+          .swiper-pagination-bullet {
+        width: 12px;
+        height: 12px;
+        text-align: center;
+        
+       
+        color: #000;
+        opacity: 1;
+        background: transparent;
+        border: 1px solid white;
+
+      }
+
+      .swiper-pagination-bullet-active {
+        border: 1px solid transparent;
+        background: #f5b754;
+      }
+      `,
+    ],
+  };
+  ngAfterViewInit() {
+    if (this.swiperEl) {
+      const swiperElement = this.swiperEl.nativeElement;
+      Object.assign(swiperElement, this.config);
+      // Ensure swiperElement has the initialize method
+      if (typeof swiperElement.initialize === 'function') {
+        swiperElement.initialize();
+      } else {
+        console.error('Swiper element does not have an initialize method.');
+      }
+    }
+  }
+}
