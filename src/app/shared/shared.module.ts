2066181b4ca2d6f92dde11fc9865c749066bb611
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-24 21:33:48 +0330

    add new bootstrap icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgIconsModule } from '@ng-icons/core';
 import { gameCarDoor } from '@ng-icons/game-icons';
 import { featherPhoneCall, featherChevronDown } from '@ng-icons/feather-icons';
+import {
+  bootstrapPersonFill,
+  bootstrapLuggageFill,
+  bootstrapCarFrontFill,
+} from '@ng-icons/bootstrap-icons';
 import { CommonModule } from '@angular/common';
 @NgModule({
   imports: [
@@ -11,6 +16,9 @@ import { CommonModule } from '@angular/common';
       gameCarDoor,
       featherPhoneCall,
       featherChevronDown,
+      bootstrapPersonFill,
+      bootstrapLuggageFill,
+      bootstrapCarFrontFill,
     }),
   ],
   exports: [CommonModule, NgIconsModule],
