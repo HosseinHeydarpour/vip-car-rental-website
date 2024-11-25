4d84ed36b1efacafafcc863338f41a23c2d703b8
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-25 21:30:43 +0330

    create and export our service section component

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -5,9 +5,16 @@ import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
 import { AboutUsComponent } from './home/about-us/about-us.component';
+import { OurServicesComponent } from './home/our-services/our-services.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent, InfoBoxComponent, AboutUsComponent],
+  declarations: [
+    HomeComponent,
+    HeroComponent,
+    InfoBoxComponent,
+    AboutUsComponent,
+    OurServicesComponent,
+  ],
   imports: [SharedModule, CoreModule],
   exports: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
