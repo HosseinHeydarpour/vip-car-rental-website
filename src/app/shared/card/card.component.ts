6df237e5d7f0395dd1cb99e11800ece627e1c09e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-25 19:35:41 +0330

    add @Input properties for imageURL and hasFade

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-
+  @Input() imageURL: string = null;
+  @Input() hasFade: boolean = false;
 }
