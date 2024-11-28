efa47bb618edbb98d412342657d2f72f469a1a0f
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-28 21:26:29 +0330

    add property Binding to car-card component to generate the cars slider with a json

--- src/app/shared/car-card/car-card.component.ts
+++ src/app/shared/car-card/car-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-car-card',
   templateUrl: './car-card.component.html',
-  styleUrl: './car-card.component.scss'
+  styleUrl: './car-card.component.scss',
 })
 export class CarCardComponent {
-
+  @Input() imagePath: string = '';
+  @Input() imageAlt: string = '';
+  @Input() carName: string = '';
+  @Input() carSeats: number = 0;
+  @Input() carTransmittion: string = '';
+  @Input() carStorage: string = '';
+  @Input() carAge: number = 0;
+  @Input() price: number = 0;
 }
