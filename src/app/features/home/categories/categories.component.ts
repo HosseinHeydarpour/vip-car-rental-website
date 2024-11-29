b422e352878f5eddc90fbcb60549f0013681c817
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-29 17:22:46 +0330

    add categories section

--- src/app/features/home/categories/categories.component.ts
+++ src/app/features/home/categories/categories.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements AfterViewInit {
+  sliderConfig: SwiperOptions = {};
+  ngAfterViewInit(): void {}
 }
