96e1e436756d1a9c48601b753ab88b6776311182
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-11-29 18:50:55 +0330

    add categories section component

--- src/app/features/home/categories/categories.component.ts
+++ src/app/features/home/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { SwiperOptions } from 'swiper/types';
 
 @Component({
@@ -7,6 +7,82 @@ import { SwiperOptions } from 'swiper/types';
   styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent implements AfterViewInit {
-  sliderConfig: SwiperOptions = {};
-  ngAfterViewInit(): void {}
+  config: SwiperOptions = {
+    navigation: false,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    slidesPerGroup: 1,
+    pagination: {
+      clickable: true,
+    },
+    injectStyles: [
+      `
+      .swiper{
+        padding-bottom: 40px;
+      }
+
+
+      .swiper-pagination{
+      position: absolute;
+      bottom: 0vh !important;
+      }
+
+          .swiper-pagination-bullet {
+        width: 12px;
+        height: 12px;
+        text-align: center;
+        
+       
+        color: #000;
+        opacity: 1;
+        background: transparent;
+        border: 1px solid white;
+
+      }
+
+      .swiper-pagination-bullet-active {
+        border: 1px solid transparent;
+        background: #f5b754;
+      }
+      `,
+    ],
+
+    breakpoints: {
+      // when window width is >= 320px
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        slidesPerGroup: 2,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        slidesPerGroup: 3,
+      },
+    },
+  };
+
+  @ViewChild('swiperEl', {
+    static: false,
+  })
+  categoriesSlider;
+  ngAfterViewInit(): void {
+    if (this.categoriesSlider) {
+      const swiperElement = this.categoriesSlider.nativeElement;
+      Object.assign(swiperElement, this.config);
+      // Ensure swiperElement has the initialize method
+      if (typeof swiperElement.initialize === 'function') {
+        swiperElement.initialize();
+      } else {
+        console.error('Swiper element does not have an initialize method.');
+      }
+    }
+  }
 }
