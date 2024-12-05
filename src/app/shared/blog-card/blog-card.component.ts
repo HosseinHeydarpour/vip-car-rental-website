4a214c16d171f10e4bd2403d3fbc0127f1f22b47
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-05 17:09:38 +0330

    add input to dynamically add title and date.. for blogs

--- src/app/shared/blog-card/blog-card.component.ts
+++ src/app/shared/blog-card/blog-card.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrl: './blog-card.component.scss'
+  styleUrl: './blog-card.component.scss',
 })
 export class BlogCardComponent {
+  showArrow: boolean = false;
 
+  @Input() title: string;
+  @Input() category: string;
+  @Input() date: {
+    day: string;
+    month: string;
+  };
+
+  @Input() blogImage: string;
+
+  onHover() {
+    this.showArrow = !this.showArrow;
+  }
 }
