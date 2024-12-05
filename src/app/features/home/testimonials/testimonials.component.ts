eca746911124ed2dea0f42eb7b30b7f91d006bb5
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-05 11:54:48 +0330

    add swiper slider to the testimonials section

---
+++ src/app/features/home/testimonials/testimonials.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { SwiperOptions } from 'swiper/types';
+
+@Component({
+  selector: 'app-testimonials',
+  templateUrl: './testimonials.component.html',
+  styleUrl: './testimonials.component.scss',
+})
+export class TestimonialsComponent implements AfterViewInit {
+  @ViewChild('swiperEl', {
+    static: false,
+  })
+  slider: ElementRef;
+  sliderConfig: SwiperOptions = {
+    navigation: false,
+    slidesPerView: 3,
+    pagination: false,
+  };
+
+  ngAfterViewInit(): void {
+    if (this.slider) {
+      const swiperElement = this.slider.nativeElement;
+      Object.assign(swiperElement, this.sliderConfig);
+      if (typeof swiperElement.initialize === 'function') {
+        swiperElement.initialize();
+      } else {
+        console.error('Swiper element does not have an initialize method.');
+      }
+    }
+  }
+}
