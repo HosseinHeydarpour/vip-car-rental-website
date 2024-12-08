5b0dcd4ce7c9fc575510dda9e8863087c118ab46
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-08 21:29:51 +0330

    add copyright section to the footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, AfterViewInit {
+  @ViewChild('date', {
+    static: false,
+  })
+  date: ElementRef;
+  ngOnInit(): void {}
 
+  ngAfterViewInit(): void {
+    this.calculateDate();
+  }
+
+  calculateDate() {
+    const year = new Date().getFullYear();
+    this.date.nativeElement.textContent = year;
+    console.log(this.date.nativeElement.textContent);
+  }
 }
