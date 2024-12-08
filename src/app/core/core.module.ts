5e383b8f82751219d0a1645fb9a27aa3011829ff
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2024-12-08 21:35:46 +0330

    add mobile nav component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MobileNavbarComponent } from './components/mobile-navbar/mobile-navbar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, MobileNavbarComponent],
   imports: [SharedModule],
   exports: [NavbarComponent, FooterComponent],
 })
